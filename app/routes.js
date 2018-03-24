0e4d95735c08c8134a3ac6d4c2ad29818a651491
jenachoi <jena.choi1106@gmail.com>
2018-03-24 19:17:32 -0400

    routes and controller for user authentication

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,25 @@ const mainController = require('../controllers/main.controller.js');
 module.exports = function(app, passport) {
     // home page
     app.get('/home', function(req, res) {
-        res.send('hello there');
+        res.send('hello there ohoho');
     });
 
-    app.get('/test', mainController.test);
+    // seed sample user
+    app.get('/seedUser', mainController.seedUser);
+
+    // process the login form
+    app.post('/login', mainController.login);
+
+    // process the signup form
+    app.post('/signup', mainController.signup);
 };
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+        return true;
+    } else {
+        res.redirect('/login');
+        return false;
+    }
+}
--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,17 +1,33 @@
 var UserProfile = require('../models/users');
 
 module.exports = {
-  test : test
+  seedUser : seedUser,
+  login : login,
+  signup : signup,
 }
 
-
-function test(req, res) {
-
+function seedUser(req, res) {
     var user = new UserProfile({username:"user1", email:"email", password:"password", events:[]});
     user.save();
 
     UserProfile.find({})
     .then(function(data) {
       console.log(data);
+    });
+}
+
+function login(req, res) {
+    passport.authenticate('local-login', {
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    });
+}
+
+function signup(req, res) {
+    passport.authenticate('local-signup', {
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
     });
 }
